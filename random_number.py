06beea1e0f60284258f3308bc19f821cbae5f11f
Prasoon Pathak <ppathak@exelixis.com>
2023-04-22 18:40:40 -0700

    added head and tail

--- random_number.py
+++ random_number.py
@@ -5,3 +5,11 @@ random_integer = random.randint(1 , 100)
 print(random_integer)
 random_decimal = float(random.random()) * 5
 print(round(random_decimal, 2))
+
+# Head and Tail
+
+number = random.randint(1 , 10)
+if number%2 == 0:
+    print("Heads")
+else:
+    print("Tails")
