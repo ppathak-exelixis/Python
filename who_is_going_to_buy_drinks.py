57d6134d1e6b3c090a0ac70c7018763c8dfbd9a9
Prasoon Pathak <ppathak@exelixis.com>
2023-04-23 09:49:38 -0700

    Changed to code

--- who_is_going_to_buy_drinks.py
+++ who_is_going_to_buy_drinks.py
@@ -6,5 +6,5 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 
 length_of_inputs = len(names)
-random_name = random.randint(-1 , length_of_inputs)
+random_name = random.randint(0 , length_of_inputs - 1)
 print(f"{names[random_name]} is going to buy the meal today!")
