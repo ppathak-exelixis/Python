98f5eeee356e6c6b32f80c572a5b4a3fe8516684
Prasoon Pathak <ppathak@exelixis.com>
2023-04-22 18:23:15 -0700

    v0.2

---
+++ random.py
@@ -0,0 +1,7 @@
+# How to generate random numbers
+import random
+
+random_interger = random.randint(1 , 100)
+print(random_interger)
+random_decimal = float(random.random()) * 5
+print(round(random_decimal, 2))
